0d67a1f441814dd98e603901fb050e762199687b
Jeff Haynie <jhaynie@appcelerator.com>
2015-01-25 11:05:39 -0800

    Initial Commit

---
+++ ios/appc-security-ios/appc-security-ios/appc_security_ios.h
@@ -0,0 +1,13 @@
+//
+//  appc_security_ios.h
+//  appc-security-ios
+//
+//  Created by Jeff Haynie on 1/25/15.
+//  Copyright (c) 2015 Jeff Haynie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface appc_security_ios : NSObject
+
+@end
---
+++ ios/appc-security-ios/appc-security-ios/appc_security_ios.m
@@ -0,0 +1,13 @@
+//
+//  appc_security_ios.m
+//  appc-security-ios
+//
+//  Created by Jeff Haynie on 1/25/15.
+//  Copyright (c) 2015 Jeff Haynie. All rights reserved.
+//
+
+#import "appc_security_ios.h"
+
+@implementation appc_security_ios
+
+@end
