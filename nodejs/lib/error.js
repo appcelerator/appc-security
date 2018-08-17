637a11a6a1c3dccd193ee2e5a0c2b37b1336f5f4
Patrick K. Clark <dev@pkclark.com>
2018-08-16 20:37:35 -0700

    bump jsonwebtoken dep to 8.X, s/jshint/eslint/

--- nodejs/lib/error.js
+++ nodejs/lib/error.js
@@ -1,8 +1,15 @@
+'use strict';
+
 /**
  * SecurityError class
+ *
+ * @param {String|*} message error message
+ * @param {String|*} code error code
+ * @param {Number|*} status status code
+ * @returns {void}
  */
 function SecurityError(message, code, status) {
-	this.constructor.prototype.__proto__ = Error.prototype;
+	Object.setPrototypeOf(this.constructor, Object.getPrototypeOf(Error));
 	Error.captureStackTrace(this, this.constructor);
 	this.name = this.constructor.name;
 	this.message = message;
