c3561dfd4fe289ec5dfb8ab5ff43fe6ff0d62df6
Ewan Harris <ewanharris93@gmail.com>
2020-09-11 14:22:48 +0100

    build: generate xcframeworks
    Part of CLI-1397

--- ios/build.sh
+++ ios/build.sh
@@ -16,6 +16,11 @@ APPC_SYMBOL_SHA1="x`random`"
 APPC_SYMBOL_DECRYPT="z`random`"
 APPC_SYMBOL_DECRYPTWITHKEY="a`random`"
 
+BUILD_DIR=build
+UNIVERSAL_OUTPUTFOLDER=${BUILD_DIR}/${LIB}-universal
+
+mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
+
 if (( $# > 0 )); then
 	CONFIG=$1
 fi
@@ -47,9 +52,20 @@ if [ ! -d boost_1_74_0 ]; then
 	tar xf boost_1_74_0.tar.gz
 fi
 
+# run tests on iOS Simulator
+xcodebuild \
+-sdk iphonesimulator \
+-configuration ${CONFIG} \
+GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS '"$(printf '%q ' "${defines[@]}")" \
+SYMROOT=./build
 
-xcodebuild -sdk iphonesimulator -configuration ${CONFIG} -target ${LIB}Tests EXCLUDED_ARCHS=arm64 GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS '"$(printf '%q ' "${defines[@]}")" SYMROOT=./build
-xcodebuild test -sdk iphonesimulator -configuration ${CONFIG} -scheme ${LIB} SYMROOT=./build -destination 'platform=iOS Simulator,name=iPhone 8,OS=13.6'
+xcodebuild test \
+-sdk iphonesimulator \
+-configuration ${CONFIG} \
+-scheme ${LIB} \
+SYMROOT=./build \
+-destination 'platform=iOS Simulator,name=iPhone 8,OS=13.6'
+
 if [ $? -ne 0 ]; then
 	exit $?
 fi
@@ -58,57 +74,61 @@ if [ -d build ]; then
 	rm -rf build
 fi
 
-
+# clean!
 xcodebuild -sdk iphonesimulator -configuration ${CONFIG} -scheme ${LIB} clean
 xcodebuild -sdk iphoneos -configuration ${CONFIG} -scheme ${LIB} clean
 
-xcodebuild -sdk iphonesimulator -configuration ${CONFIG} -target ${LIB} EXCLUDED_ARCHS=arm64 GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS '"$(printf '%q ' "${defines[@]}")"
-xcodebuild -sdk iphoneos -configuration ${CONFIG} -target ${LIB} GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS '"$(printf '%q ' "${defines[@]}")"
-
-lipo build/${CONFIG}-iphonesimulator/lib${LIB}.a build/${CONFIG}-iphoneos/lib${LIB}.a -create -output build/lib${LIB}.a
-
-if [ $? -ne 0 ]; then
-	exit $?
-fi
-
-for arch in armv7 arm64 i386 x86_64; do
-	xcrun -sdk iphoneos lipo build/lib${LIB}.a -verify_arch $arch
-	if (( $? != 0 )); then
-		echo -e "\033[31mERROR:\033[0m YOU DID NOT BUILD IN SYMBOLS FOR $arch"
-		exit 1
-	fi
-done
-
-xcrun -sdk iphoneos lipo -info build/lib${LIB}.a
-mv build/lib${LIB}.a build/lib${FINALNAME}.a
-
 findsymbol() {
-	otool -Sv build/lib${LIB}.a | grep $1 | wc -l | sed 's/[ \s\t]*//g'
+	# otool -Sv build/lib${LIB}.a | grep $1 | wc -l | sed 's/[ \s\t]*//g'
+	xcrun llvm-nm -print-armap ${2}/Products/usr/local/lib/lib${LIB}.a  | grep $1 | wc -l | sed 's/[ \s\t]*//g'
 }
 
-if [ `findsymbol decrypt` -ne 0 ]; then
-	echo -e "\033[31mERROR:\033[0m should not have found decrypt symbol in final binary. obfuscation failed!"
-	exit 1
-fi
+buildArchive() {
+	ARCHIVE_PATH=${BUILD_DIR}/$1.xcarchive
+	xcodebuild archive \
+	-sdk $1 \
+	-scheme ${LIB} \
+	-archivePath ${ARCHIVE_PATH} \
+	SKIP_INSTALL=NO \
+	BUILD_LIBRARIES_FOR_DISTRIBUTION=YES \
+	GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS '"$(printf '%q ' "${defines[@]}")" \
+	$2
 
-if [ `findsymbol hmac256` -ne 0 ]; then
-	echo -e "\033[31mERROR:\033[0m should not have found hmac256 symbol in final binary. obfuscation failed!"
-	exit 1
-fi
+	if [ `findsymbol decrypt $ARCHIVE_PATH` -ne 0 ]; then
+		echo -e "\033[31mERROR:\033[0m should not have found decrypt symbol in final binary $ARCHIVE_PATH. obfuscation failed!"
+		exit 1
+	fi
 
-if [ `findsymbol sha1` -ne 0 ]; then
-	echo -e "\033[31mERROR:\033[0m should not have found sha1 symbol in final binary. obfuscation failed!"
-	exit 1
-fi
+	if [ `findsymbol hmac256 $ARCHIVE_PATH` -ne 0 ]; then
+		echo -e "\033[31mERROR:\033[0m should not have found hmac256 symbol in final binary $ARCHIVE_PATH. obfuscation failed!"
+		exit 1
+	fi
 
-# copy and replace our build tokens in our header
-cp appcsecurity/appcsecurity.h build
-sed -i -e "s/APPC_SYMBOL_HMAC256/${APPC_SYMBOL_HMAC256}/" build/appcsecurity.h
-sed -i -e "s/APPC_SYMBOL_SHA1/${APPC_SYMBOL_SHA1}/" build/appcsecurity.h
-sed -i -e "s/APPC_SYMBOL_DECRYPTWITHKEY/${APPC_SYMBOL_DECRYPTWITHKEY}/" build/appcsecurity.h
-sed -i -e "s/APPC_SYMBOL_DECRYPT/${APPC_SYMBOL_DECRYPT}/" build/appcsecurity.h
+	if [ `findsymbol sha1 $ARCHIVE_PATH` -ne 0 ]; then
+		echo -e "\033[31mERROR:\033[0m should not have found sha1 symbol in final binary $ARCHIVE_PATH. obfuscation failed!"
+		exit 1
+	fi
 
-echo -e "\033[32mBuilt static library located at \033[0m\033[33mbuild/lib${FINALNAME}.a\033[0m"
-echo -e "\033[32mBuilt header located at \033[0m\033[33mbuild/${FINALNAME}.h\033[0m"
+	HEADER_PATH=$ARCHIVE_PATH/Products/usr/local/include/$LIB.h
+	# copy and replace our build tokens in our header
+	sed -i -e "s/APPC_SYMBOL_HMAC256/${APPC_SYMBOL_HMAC256}/" $HEADER_PATH
+	sed -i -e "s/APPC_SYMBOL_SHA1/${APPC_SYMBOL_SHA1}/" $HEADER_PATH
+	sed -i -e "s/APPC_SYMBOL_DECRYPTWITHKEY/${APPC_SYMBOL_DECRYPTWITHKEY}/" $HEADER_PATH
+	sed -i -e "s/APPC_SYMBOL_DECRYPT/${APPC_SYMBOL_DECRYPT}/" $HEADER_PATH
+}
+
+buildArchive iphonesimulator
+buildArchive iphoneos
+buildArchive macosx SUPPORTS_MACCATALYST=YES
+
+# Build the XCFramework
+xcodebuild -create-xcframework \
+-library $BUILD_DIR/iphonesimulator.xcarchive/Products/usr/local/lib/lib${LIB}.a \
+-headers $BUILD_DIR/iphonesimulator.xcarchive/Products/usr/local/include/ \
+-library $BUILD_DIR/iphoneos.xcarchive/Products/usr/local/lib/lib${LIB}.a \
+-headers $BUILD_DIR/iphoneos.xcarchive/Products/usr/local/include/ \
+-library $BUILD_DIR/macosx.xcarchive/Products/usr/local/lib/lib${LIB}.a \
+-headers $BUILD_DIR/macosx.xcarchive/Products/usr/local/include/ \
+-output ${UNIVERSAL_OUTPUTFOLDER}/${LIB}.xcframework
+
 exit 0
-
